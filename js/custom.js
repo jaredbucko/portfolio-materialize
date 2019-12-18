f61d54f04247913b54667a60984aa50ed273df45
jaredbucko <jaredbucko@icloud.com>
2019-12-18 10:59:29 -0800

    imported site from local working directory

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// materialize navbar
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
